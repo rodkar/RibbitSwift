4c10f9992f69e92516442e4aaa55432b4ffe8249
2013_13_mbp <rod@rodkar.com>
2014-10-07 19:55:05 +0800

    update from 17

--- RibbitSwift/CameraViewController.swift
+++ RibbitSwift/CameraViewController.swift
@@ -12,29 +12,30 @@ import MobileCoreServices
 
 class CameraViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var imagePicker = UIImagePickerController()
+    let imagePicker = UIImagePickerController()
+    var image : UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.imagePicker.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(true)
+        super.viewWillAppear(animated)
+        self.imagePicker.delegate = self
+        self.imagePicker.allowsEditing = false
+        self.imagePicker.videoMaximumDuration = 10
         
-        imagePicker.allowsEditing = false
-        
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-        }  else {
-            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        }
-        
-        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(imagePicker.sourceType)!
-        self.presentViewController(imagePicker, animated: false, completion: nil)
+//        if self.image == nil{
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+                self.imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            }  else {
+                self.imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            }
+            
+            self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(self.imagePicker.sourceType)!
+            self.presentViewController(self.imagePicker, animated: false, completion: nil)
+//        }
     }
-    
-    
     
     // MARK: - Table view data source
     
@@ -53,18 +54,33 @@ class CameraViewController: UITableViewController, UIImagePickerControllerDelega
     
     // MARK: - Image Picker Controller Delegate
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-
         self.dismissViewControllerAnimated(false, completion: nil)
         self.tabBarController?.selectedIndex = 0
-
     }
-
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        let mediaType = info
-//            .indexForKey(UIImagePickerControllerMediaType)
-        println("media type is: \(mediaType.indexForKey(UIImagePickerControllerMediaType))")
+        let mediaType = info[UIImagePickerControllerMediaType] as NSString
         
+        if mediaType == kUTTypeImage {
+            // a photo was taken/selected!
+            self.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+            
+            if self.imagePicker.sourceType == UIImagePickerControllerSourceType.Camera {
+                // save the image
+                UIImageWriteToSavedPhotosAlbum(self.image, nil, nil, nil)
+            }
+            
+        } else {
+            // a video was taken/selected
+            let imagePickerURL = info[UIImagePickerControllerMediaURL] as? NSURL
+            let videoFilePath = imagePickerURL?.path
+            if self.imagePicker.sourceType == UIImagePickerControllerSourceType.Camera {
+                // save the video
+                if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(videoFilePath){
+                    UISaveVideoAtPathToSavedPhotosAlbum(videoFilePath, nil, nil, nil)
+                }
+            }
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
-
 }
